7eddde133680226ab0bf1d2adfef4941bc84a6f2
alantheandroid <alessio.ilardi@gmail.com>
2022-07-10 11:45:05 +0200

    fix: ReactDOM import statement is now correct

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* import { HelloWorld } from "./HelloWorld"; */
 import { App } from "./App";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 /* const helloWorldElement = <HelloWorld />; */
 const root = document.querySelector("#root");
