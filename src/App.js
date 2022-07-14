2bed649f9d7dfd2355649bb945882959641159e9
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-07-14 11:28:27 +0200

    feat: first iteration of ternary operator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
-/* import { Welcome } from "./Welcome"; */
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
         <h1>My Awesome Application</h1>
-        <HelloWorld renderGreeting={true} />
+        <Welcome />
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,6 +2,14 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-    return <h2>Hello, {this.props.name}</h2>;
+    return (
+      <div>
+        {this.props.name ? (
+          <h2>Hello, {this.props.name}</h2>
+        ) : (
+          <h2>Hello World!</h2>
+        )}
+      </div>
+    );
   }
 }
