c41a1f26ae212580e47470bfc85d3b6bfc597a13
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-07-17 12:53:59 +0200

    feat: add Counter component [NEEDS FIX]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Clock } from "./Clock";
-/* import { HelloWorld } from "./HelloWorld"; */
+import { Counter } from "./Counter";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
@@ -9,6 +9,7 @@ export class App extends React.Component {
       <div>
         <h1>My Awesome Application</h1>
         <Welcome name="Jimmy" />
+        <Counter initialValue={30} incrementBy={2} timeout={500} />
         <Clock />
       </div>
     );
---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class Counter extends React.Component {
+  state = {
+    count: this.props.initialValue ?? 0,
+  };
+
+  constructor(props) {
+    super(props);
+
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + (this.props.incrementBy ?? 1),
+        };
+      }, this.props.timeout ?? 1000);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Count: {this.state.count}</h3>
+      </div>
+    );
+  }
+}
